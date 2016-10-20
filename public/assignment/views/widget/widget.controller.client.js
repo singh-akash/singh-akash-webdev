4f5103878259d0731ca25b5ef9e3a66fe4d2e015
Akash <singh.aka@husky.neu.edu>
2016-10-20 00:09:51 -0700

    Adding widget edit features

--- public/assignment/views/widget/widget.controller.client.js
+++ public/assignment/views/widget/widget.controller.client.js
@@ -30,16 +30,42 @@
         }
     }
 
-    function NewWidgetController($routeParams, WidgetService) {
+    function NewWidgetController($routeParams, $location, WidgetService) {
         var vm = this;
+        vm.addWidget = addWidget;
+
+        vm.userId = parseInt($routeParams['uid']);
+        vm.websiteId = parseInt($routeParams['wid']);
+        vm.pageId = parseInt($routeParams['pid']);
+
+        function addWidget(widgetType) {
+            WidgetService.create(vm.pageId, {widgetType: widgetType});
+            vm.widgetId = PageService.createPage(vm.websiteId, page);
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId +
+                "/page" + vm.pageId + "/widget" + vm.widgetId);
+        }
     }
 
-    function EditWidgetController($routeParams, WidgetService) {
+    function EditWidgetController($routeParams, $location, WidgetService) {
         var vm = this;
+
+        vm.updateWidget = updateWidget;
+        vm.deleteWidget = deleteWidget;
+
         vm.userId  = parseInt($routeParams.uid);
         vm.websiteId  = parseInt($routeParams.wid);
         vm.pageId  = parseInt($routeParams.pid);
         vm.widgetId = parseInt($routeParams.wgid);
         vm.widget = WidgetService.findWidgetById(vm.widgetId);
+
+        function deleteWidget() {
+            WidgetService.deleteWidget(vm.widgetId);
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
+        }
+
+        function updateWidget(widget) {
+            WidgetService.updateWidget(vm.widgetId, widget);
+            $location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page/" + vm.pageId + "/widget");
+        }
     }
 })();
